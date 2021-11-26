6be0e47ff2ce9eb0d42f02f60fbaec9ad636bbbb
François Guérin <francois.guerin@gravity.earth>
2021-11-26 20:13:43 +0100

    Implement resolution from TZIP-19 DID Manager

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
-import { ChainIds } from "@taquito/taquito";
-import { Prefix, validateAddress, ValidationResult } from "@taquito/utils";
+import { TezosToolkit } from "@taquito/taquito";
+import { Tzip16Module } from "@taquito/tzip16";
+import { Prefix } from "@taquito/utils";
 import {
-  DIDDocument,
   DIDResolutionOptions,
   DIDResolutionResult,
   DIDResolver,
   ParsedDID,
   Resolver,
-  VerificationMethod,
 } from "did-resolver";
-
-enum TezosNetworks {
-  GRANADANET = "granadanet",
-  MAINNET = "mainnet",
-}
-
-function validateNetwork(network: any): boolean {
-  return Object.values(TezosNetworks).includes(network as TezosNetworks);
-}
+import { update as updateLayer1 } from "./layer1";
+import { update as updateLayer2 } from "./layer2";
+import { validateIdentifier } from "./utils";
 
 async function resolve(
   did: string,
@@ -25,73 +18,47 @@ async function resolve(
   _didResolver: Resolver,
   _options: DIDResolutionOptions
 ): Promise<DIDResolutionResult> {
-  const EMPTY_RESULT: DIDResolutionResult = {
+  const result: DIDResolutionResult = {
     didResolutionMetadata: {},
     didDocument: null,
     didDocumentMetadata: {},
   };
 
-  const arr = parsed.id.split(":");
-
-  if (arr.length !== 1 && arr.length !== 2) {
+  const validIdentifier = validateIdentifier(parsed.id);
+  if (!validIdentifier) {
     return {
-      ...EMPTY_RESULT,
+      ...result,
       didResolutionMetadata: { error: "invalidDid" },
     };
   }
 
-  const address = arr[arr.length - 1];
-  const validAddress = validateAddress(address);
-
-  if (validAddress !== ValidationResult.VALID) {
-    return {
-      ...EMPTY_RESULT,
-      didResolutionMetadata: { error: "invalidDid" },
-    };
-  }
-
-  const network = arr.length === 2 ? arr[0] : TezosNetworks.MAINNET;
-  const validNetwork = validateNetwork(network);
-
-  if (!validNetwork) {
-    return {
-      ...EMPTY_RESULT,
-      didResolutionMetadata: { error: "invalidDid" },
-    };
-  }
+  const { network, address } = validIdentifier;
 
   if (parsed.id.startsWith(Prefix.KT1)) {
     return {
-      ...EMPTY_RESULT,
+      ...result,
       didResolutionMetadata: { error: "notImplemented" },
     };
   }
 
-  const chainId = ChainIds[network.toUpperCase() as "MAINNET" | "GRANADANET"];
-
-  const verificationMethod: VerificationMethod = {
-    id: `${did}#blockchainAccountId`,
-    type: "Ed25519PublicKeyBLAKE2BDigestSize20Base58CheckEncoded2021",
-    controller: did,
-    blockchainAccountId: `tezos:${chainId}:${address}`,
-  };
-
-  const didDocument: DIDDocument = {
+  result.didDocument = {
     "@context": [
       "https://www.w3.org/ns/did/v1",
       "https://w3id.org/security/v1",
     ],
     id: did,
-    verificationMethod: [verificationMethod],
-    authentication: [verificationMethod.id],
-    assertionMethod: [verificationMethod.id],
   };
 
-  return {
-    ...EMPTY_RESULT,
-    didDocument,
-    didResolutionMetadata: { contentType: "application/did+ld+json" },
-  };
+  const rpc = `https://${network}.api.tez.ie`;
+  const tezosToolkit = new TezosToolkit(rpc);
+  tezosToolkit.addExtension(new Tzip16Module());
+  const indexer = `https://api.${network}.tzkt.io`;
+
+  await updateLayer1(tezosToolkit, result, { address });
+
+  await updateLayer2(tezosToolkit, result, { indexer, address });
+
+  return result;
 }
 
 export default {
