e5b3209ee45316d2cb1b2f15049890c761eb4b98
François Guérin <francois.guerin@gravity.earth>
2021-12-05 12:25:43 +0100

    Test layer3

--- test/resolver.ts
+++ test/resolver.ts
@@ -1,8 +1,15 @@
 import { expect } from "chai";
 import { Resolver } from "did-resolver";
 import * as faker from "faker";
-
+import { importJWK, SignJWT } from "jose";
 import tz from "../src/index";
+
+const jwk = {
+  kty: "OKP",
+  crv: "Ed25519",
+  x: "ROm8DWLwygV95uSyAafOsjdRWCTAKu-Hfa4IFBkODtQ",
+  d: "l6Oqs9z3qB9XQZrJvw2KPuvvQDNV0pU2AnuKN30yXLA",
+};
 
 describe("DID Resolver", function () {
   let resolver: Resolver;
@@ -91,44 +98,285 @@ describe("DID Resolver", function () {
       expect(result.didDocumentMetadata).to.be.an("object").and.to.be.empty;
     });
 
-    it("should successfully resolve an implied DID Document from a valid DID", async function () {
-      const did = "did:tz:granadanet:tz1TzrmTBSuiVHV2VfMnGRMYvTEPCP42oSM8";
+    describe("Layer 1", function () {
+      it("should successfully resolve an implied DID Document from a valid DID", async function () {
+        const did = "did:tz:granadanet:tz1TzrmTBSuiVHV2VfMnGRMYvTEPCP42oSM8";
 
-      const result = await resolver.resolve(did);
+        const result = await resolver.resolve(did);
 
-      expect(result)
-        .to.be.an("object")
-        .and.to.have.keys(
-          "didResolutionMetadata",
-          "didDocument",
-          "didDocumentMetadata"
-        );
-      expect(result.didResolutionMetadata)
-        .to.be.an("object")
-        .and.to.not.have.property("error");
-      expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
-      expect(result.didDocumentMetadata).to.be.an("object").and.to.be.empty;
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.not.have.property("error");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocument)
+          .to.have.property("authentication")
+          .and.to.be.instanceOf(Array)
+          .and.to.have.lengthOf(1)
+          .and.to.have.property("0")
+          .to.be.a("string");
+        expect(result.didDocument)
+          .to.have.property("assertionMethod")
+          .and.to.be.instanceOf(Array)
+          .and.to.have.lengthOf(1)
+          .and.to.have.property("0")
+          .to.be.a("string");
+        expect(result.didDocumentMetadata).to.be.an("object").and.to.be.empty;
+      });
+
+      it("should successfully add a public key to an implied document", async function () {
+        const did = "did:tz:granadanet:tz1TzrmTBSuiVHV2VfMnGRMYvTEPCP42oSM8";
+        const publicKey =
+          "edpkuix6Lv8vnrz6uDe1w8uaXY7YktitAxn6EHdy2jdzq5n5hZo94n";
+
+        const result = await resolver.resolve(did, { publicKey });
+
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.not.have.property("error");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocument)
+          .to.have.property("authentication")
+          .and.to.be.instanceOf(Array)
+          .and.to.have.lengthOf(1)
+          .and.to.have.property("0")
+          .to.be.an("object")
+          .and.to.have.property("publicKeyBase58")
+          .to.equal(publicKey);
+        expect(result.didDocument)
+          .to.have.property("assertionMethod")
+          .and.to.be.instanceOf(Array)
+          .and.to.have.lengthOf(1)
+          .and.to.have.property("0")
+          .to.be.a("string");
+        expect(result.didDocumentMetadata).to.be.an("object").and.to.be.empty;
+      });
     });
 
-    it("should successfully resolve a deployed DID Document from a valid DID", async function () {
-      const did = "did:tz:granadanet:tz1Mmhk4yVqnvKkciEgqDBjwNDAn7DtWaPkG";
+    describe("Layer 2", function () {
+      it("should successfully resolve a deployed DID Document from a valid DID", async function () {
+        const did = "did:tz:granadanet:tz1Mmhk4yVqnvKkciEgqDBjwNDAn7DtWaPkG";
 
-      const result = await resolver.resolve(did);
+        const result = await resolver.resolve(did);
 
-      expect(result)
-        .to.be.an("object")
-        .and.to.have.keys(
-          "didResolutionMetadata",
-          "didDocument",
-          "didDocumentMetadata"
-        );
-      expect(result.didResolutionMetadata)
-        .to.be.an("object")
-        .and.to.not.have.property("error");
-      expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
-      expect(result.didDocumentMetadata)
-        .to.be.an("object")
-        .and.to.have.keys("created", "updated");
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.not.have.property("error");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocumentMetadata)
+          .to.be.an("object")
+          .and.to.have.keys("created", "updated");
+      });
+    });
+
+    describe("Layer 3", function () {
+      it("should fail applying a patch without public key", async function () {
+        const signedIetfJsonPatch =
+          "eyJhbGciOiJFZERTQSIsImtpZCI6ImRpZDp0ejpkZWxwaGluZXQ6dHoxV3Z2YkVHcEJYR2VUVmJMaVI2RFlCZTFpem1naVl1WmJxI2Jsb2NrY2hhaW5BY2NvdW50SWQifQ.eyJpZXRmLWpzb24tcGF0Y2giOiBbCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICB7CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIm9wIjogImFkZCIsCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInBhdGgiOiAiL3NlcnZpY2UvMSIsCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInZhbHVlIjogewogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAiaWQiOiAidGVzdF9zZXJ2aWNlX2lkIiwKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInR5cGUiOiAidGVzdF9zZXJ2aWNlIiwKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInNlcnZpY2VFbmRwb2ludCI6ICJ0ZXN0X3NlcnZpY2VfZW5kcG9pbnQiCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfQogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfQogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICBdfQ.OTMe8ljEZEqZrdfkL1hhuiVXFGw_taFRVqNTfsycxFDq5FPu1ZSgaTOertyC61cQQXNLqTRo2kHAos8kx8PHAQ";
+        const did = "did:tz:granadanet:tz1PXpQSpk8kytvLfX2or39jwEmX5smpDYxi";
+
+        const result = await resolver.resolve(did, { signedIetfJsonPatch });
+
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.have.property("error")
+          .and.to.be.a("string")
+          .and.to.equal("Need public key for signed patches");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocumentMetadata)
+          .to.be.an("object")
+          .and.to.have.keys("created", "updated");
+      });
+
+      it("should fail applying a patch with a missmatch public key", async function () {
+        const signedIetfJsonPatch =
+          "eyJhbGciOiJFZERTQSIsImtpZCI6ImRpZDp0ejpkZWxwaGluZXQ6dHoxV3Z2YkVHcEJYR2VUVmJMaVI2RFlCZTFpem1naVl1WmJxI2Jsb2NrY2hhaW5BY2NvdW50SWQifQ.eyJpZXRmLWpzb24tcGF0Y2giOiBbCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICB7CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIm9wIjogImFkZCIsCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInBhdGgiOiAiL3NlcnZpY2UvMSIsCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInZhbHVlIjogewogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAiaWQiOiAidGVzdF9zZXJ2aWNlX2lkIiwKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInR5cGUiOiAidGVzdF9zZXJ2aWNlIiwKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgInNlcnZpY2VFbmRwb2ludCI6ICJ0ZXN0X3NlcnZpY2VfZW5kcG9pbnQiCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfQogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfQogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICBdfQ.OTMe8ljEZEqZrdfkL1hhuiVXFGw_taFRVqNTfsycxFDq5FPu1ZSgaTOertyC61cQQXNLqTRo2kHAos8kx8PHAQ";
+        const publicKey =
+          "edpkuix6Lv8vnrz6uDe1w8uaXY7YktitAxn6EHdy2jdzq5n5hZo94n";
+        const did = "did:tz:granadanet:tz1PXpQSpk8kytvLfX2or39jwEmX5smpDYxi";
+
+        const result = await resolver.resolve(did, {
+          publicKey,
+          signedIetfJsonPatch,
+        });
+
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.have.property("error")
+          .and.to.be.a("string");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocumentMetadata)
+          .to.be.an("object")
+          .and.to.have.keys("created", "updated");
+      });
+
+      it("should fail applying a patch without header property 'kid'", async function () {
+        const did = "did:tz:granadanet:tz1PXpQSpk8kytvLfX2or39jwEmX5smpDYxi";
+        const header = {
+          alg: "EdDSA",
+        };
+        const payload = {
+          "ietf-json-patch": [
+            {
+              op: "replace",
+              path: "/service/0",
+              value: {
+                id: "test_service_id",
+                type: "test_service",
+                serviceEndpoint: "test_service_endpoint",
+              },
+            },
+          ],
+        };
+        const publicKey =
+          "edpkuAaEA2hfytsz5gfqGWqj1f8md5HLgESDoaKq5eShGEw6okXXLn";
+        const jwt = new SignJWT(payload);
+        jwt.setProtectedHeader(header);
+        const key = await importJWK(jwk, "EdBlake2b");
+        const signedIetfJsonPatch = await jwt.sign(key);
+
+        const result = await resolver.resolve(did, {
+          publicKey,
+          signedIetfJsonPatch,
+        });
+
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.have.property("error")
+          .and.to.be.a("string")
+          .and.to.equal("Missing 'kid' header property");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocumentMetadata)
+          .to.be.an("object")
+          .and.to.have.keys("created", "updated");
+      });
+
+      it("should fail applying a patch with a 'kid' that does not appear on the DID Document", async function () {
+        const did = "did:tz:granadanet:tz1PXpQSpk8kytvLfX2or39jwEmX5smpDYxi";
+        const header = {
+          alg: "EdDSA",
+          kid: "hello",
+        };
+        const payload = {
+          "ietf-json-patch": [
+            {
+              op: "replace",
+              path: "/service/0",
+              value: {
+                id: "test_service_id",
+                type: "test_service",
+                serviceEndpoint: "test_service_endpoint",
+              },
+            },
+          ],
+        };
+        const publicKey =
+          "edpkuAaEA2hfytsz5gfqGWqj1f8md5HLgESDoaKq5eShGEw6okXXLn";
+        const jwt = new SignJWT(payload);
+        jwt.setProtectedHeader(header);
+        const key = await importJWK(jwk, "EdBlake2b");
+        const signedIetfJsonPatch = await jwt.sign(key);
+
+        const result = await resolver.resolve(did, {
+          publicKey,
+          signedIetfJsonPatch,
+        });
+
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.have.property("error")
+          .and.to.be.a("string")
+          .and.to.equal("Not authorized to propose updates");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocumentMetadata)
+          .to.be.an("object")
+          .and.to.have.keys("created", "updated");
+      });
+
+      it("should fail applying a patch without payload property 'ietf-json-patch'", async function () {
+        const did = "did:tz:granadanet:tz1PXpQSpk8kytvLfX2or39jwEmX5smpDYxi";
+        const header = {
+          alg: "EdDSA",
+          kid: "did:tz:granadanet:tz1PXpQSpk8kytvLfX2or39jwEmX5smpDYxi#blockchainAccountId",
+        };
+        const payload = {};
+        const publicKey =
+          "edpkuAaEA2hfytsz5gfqGWqj1f8md5HLgESDoaKq5eShGEw6okXXLn";
+        const jwt = new SignJWT(payload);
+        jwt.setProtectedHeader(header);
+        const key = await importJWK(jwk, "EdBlake2b");
+        const signedIetfJsonPatch = await jwt.sign(key);
+
+        const result = await resolver.resolve(did, {
+          publicKey,
+          signedIetfJsonPatch,
+        });
+
+        expect(result)
+          .to.be.an("object")
+          .and.to.have.keys(
+            "didResolutionMetadata",
+            "didDocument",
+            "didDocumentMetadata"
+          );
+        expect(result.didResolutionMetadata)
+          .to.be.an("object")
+          .and.to.have.property("error")
+          .and.to.be.a("string")
+          .and.to.equal("Missing 'ietf-json-patch' payload property");
+        expect(result.didDocument).to.be.an("object").and.to.not.be.empty;
+        expect(result.didDocumentMetadata)
+          .to.be.an("object")
+          .and.to.have.keys("created", "updated");
+      });
     });
   });
 });
