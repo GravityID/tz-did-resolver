14baef29a3de9c65b628ae46131858e589e2b9e0
François Guérin <francois.guerin@gravity.earth>
2021-12-12 09:42:02 +0100

    Add function isTzip019

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
-import { ChainIds } from "@taquito/taquito";
+import { ChainIds, TezosToolkit } from "@taquito/taquito";
+import { tzip16 } from "@taquito/tzip16";
 import {
+  Prefix,
   validateAddress as _validateAddress,
   ValidationResult,
 } from "@taquito/utils";
@@ -23,6 +25,26 @@ export function validateNetwork(network: string): boolean {
 
 export function validateAddress(address: string): boolean {
   return _validateAddress(address) === ValidationResult.VALID;
+}
+
+export async function isTzip019(
+  tezosToolkit: TezosToolkit,
+  address: string
+): Promise<boolean> {
+  if (!validateAddress(address)) return false;
+  if (!address.startsWith(Prefix.KT1)) return false;
+
+  const contract = await tezosToolkit.contract.at(address, tzip16);
+
+  const { metadata } = await contract.tzip16().getMetadata();
+  const views = await contract.tzip16().metadataViews();
+
+  if (!metadata.interfaces) return false;
+  if (!metadata.interfaces.includes("TZIP-019")) return false;
+  if (!(views.GetVerificationMethod instanceof Function)) return false;
+  if (!(views.GetService instanceof Function)) return false;
+
+  return true;
 }
 
 export function validateIdentifier(id: string):
